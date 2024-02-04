19165d7a0ad7d5bd6387730b3d6d85ed10bcec5b
tejhande <59686002+tejhande@users.noreply.github.com>
2024-02-04 11:18:57 +0530

    Add files via upload

---
+++ Landing_Page.js
@@ -0,0 +1,28 @@
+function openNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "50%";
+}
+function closeNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "0%";
+}
+
+function showMapsAlert() {
+	alert("Redirecting To Google Maps!");
+  }
+  
+  document.getElementById("explore_button").addEventListener("click", showMapsAlert);
+  
+function showWebsiteAlert() {
+	alert("Redirecting To Creators Portfolio Website!");
+  }
+  
+  document.getElementById("website").addEventListener("click", showWebsiteAlert);
+  
+function myFunc(){
+	alert(`Welcome To Marsstrong.com`);
+}
+
+// Tejas Hande
+// tejasamolhande@gmail.com
+// 8600828734
